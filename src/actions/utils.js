16d3b9d0eec59b23c21fe5f769f4cb56789b7e16
ArtixZ <artixzhu@gmail.com>
2017-11-19 15:34:25 -0500

    add user info to each every query

--- src/actions/utils.js
+++ src/actions/utils.js
@@ -2,7 +2,7 @@ import {
     FOOD_CLASS_FROM_IMAGE
 } from './urls';
 import { Constants, Location, Permissions } from 'expo';
-
+import firebase from 'firebase';
 
 export const callAPI = async (method, {requestedType, url}, data) => {
     
@@ -10,11 +10,26 @@ export const callAPI = async (method, {requestedType, url}, data) => {
     if (status !== 'granted') {
         
     }
+// 
+    const user = firebase.auth().currentUser;
+    let name, email, photoUrl, uid, emailVerified;
+    
+    if (user != null) {
+      name = user.displayName;
+      email = user.email;
+      photoUrl = user.photoURL;
+      emailVerified = user.emailVerified;
+      uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+                       // this value to authenticate with your backend server, if
+                       // you have one. Use User.getToken() instead.
+    }
+    const userInfo = {name, email, photoUrl, uid, emailVerified};
+// 
 
     const location = await Location.getCurrentPositionAsync({});
 
     data["requested_types"] = requestedType;
-    data = {...data, ...location};
+    data = {...data, ...location, user_info: userInfo};
     
     return fetch(url, {
         method: method,
